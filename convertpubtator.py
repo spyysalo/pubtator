974bd8550be6b388328ba83dc825e1ec43a9322e
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2016-02-04 09:13:57 +0000

    JSON output

--- convertpubtator.py
+++ convertpubtator.py
@@ -7,11 +7,15 @@ import re
 import codecs
 import collections
 import itertools
+import json
 
 from os import path
 from logging import warn
 
 DEFAULT_ENCODING = 'utf-8'
+
+FORMATS = ['standoff', 'json']
+DEFAULT_FORMAT = 'standoff'
 
 # Regular expressions matching PubTator format embedded text, span
 # annotation, and relation annotation.
@@ -22,10 +26,12 @@ REL_RE = re.compile(r'^(\d+)\t(\S+)\t(\S+)\t(\S+)\s*$')
 def argparser():
     import argparse
     ap = argparse.ArgumentParser()
-    ap.add_argument('-o', '--output', metavar='DIR', default=None,
-                    help='Output directory')
     ap.add_argument('-e', '--encoding', default=DEFAULT_ENCODING,
                     help='Encoding (default %s)' % DEFAULT_ENCODING)
+    ap.add_argument('-f', '--format', default=DEFAULT_FORMAT, choices=FORMATS,
+                    help='Output format (default %s)' % DEFAULT_FORMAT)
+    ap.add_argument('-o', '--output', metavar='DIR', default=None,
+                    help='Output directory')
     ap.add_argument('files', metavar='FILE', nargs='+',
                     help='Input PubTator files')
     return ap
@@ -33,22 +39,65 @@ def argparser():
 class ParseError(Exception):
     pass
 
+def pretty_dumps(obj):
+    return json.dumps(obj, sort_keys=True, indent=2, separators=(',', ': '))
+
+def encoding(options):
+    try:
+        return options.encoding
+    except:
+        return DEFAULT_ENCODING
+
 def next_id(prefix, ann_by_id):
     for i in itertools.count(1):
         if prefix+str(i) not in ann_by_id:
             return prefix+str(i)
 
+def guess_namespace(type_):
+    if type_ == 'Species':
+        return 'taxonomy'
+    elif type_ == 'Gene':
+        return 'EGID'    # Entrez gene ID
+    elif type_ == 'Chemical':
+        return 'MESH'
+    else:
+        return 'unknown'
+
 class SpanAnnotation(object):
     def __init__(self, docid, start, end, text, type_, norm, substrings):
         self.docid = docid
-        self.start = start
-        self.end = end
+        self.start = int(start)
+        self.end = int(end)
         self.text = text
         self.type = type_
-        self.norm = norm
+        self._norm = norm
         # substrings capture cases such as
         #     2234245	314	341	visual or auditory toxicity	Disease	D014786|D006311	visual toxicity|auditory toxicity
         self.substrings = substrings
+
+    @property
+    def norm(self):
+        if not self._norm.strip():
+            return None
+        elif ':' in self._norm:
+            return self._norm
+        else:
+            # Norm value, but no namespace; add a guess
+            return guess_namespace(self.type) + ':' + self._norm
+
+    def to_dict(self):
+        d = {
+            'start': self.start,
+            'end': self.end,
+            'text': self.text,
+            'type': self.type,
+        }
+        if self.norm:
+            d['norm'] = self.norm
+        return d
+
+    def to_json(self):
+        return pretty_dumps(self.to_dict())
 
     def to_ann_lines(self, ann_by_id=None):
         # TODO: substrings
@@ -58,13 +107,11 @@ class SpanAnnotation(object):
         t_ann = '%s\t%s %s %s\t%s' % (
             tid, self.type, self.start, self.end, self.text)
         ann_by_id[tid] = t_ann
-        if not self.norm.strip():
+        if not self.norm:
             return [t_ann]    # No norm
         else:
-            # If the norm ID lacks a namespace, just add something
             nid = next_id('N', ann_by_id)
-            norm = self.norm if ':' in self.norm else 'Unknown:%s' % self.norm
-            n_ann = '%s\tReference %s %s\t%s' % (nid, tid, norm, self.text)
+            n_ann = '%s\tReference %s %s\t%s' % (nid, tid, self.norm, self.text)
             ann_by_id[nid] = n_ann
             return [t_ann, n_ann]
         
@@ -82,6 +129,12 @@ class RelationAnnotation(object):
         self.arg1 = arg1
         self.arg2 = arg2
 
+    def to_dict(self):
+        raise NotImplementedError()    # TODO
+
+    def to_json(self):
+        return pretty_dumps(self.to_dict())
+
     def to_ann_lines(self, idseq=0):
         # No direct support for document-level relation annotations in
         # .ann output format; skip for now.
@@ -95,10 +148,44 @@ class RelationAnnotation(object):
         return cls(*m.groups())
 
 class PubTatorDocument(object):
-    def __init__(self, id_, text, annotations):
+    def __init__(self, id_, text_sections, annotations):
         self.id = id_
-        self.text = text
+        self.text_sections = text_sections
         self.annotations = annotations
+
+    @property
+    def text(self):
+        return '\n'.join(text for label, text in self.text_sections)
+
+    @property
+    def title(self):
+        return ' '.join(text for label, text in self.text_sections
+                        if label == 't')
+
+    def text_dict(self):
+        return {
+            '_id': self.id,
+            'title': self.title,
+            'abstract': [ {
+                'text': text
+            } for label, text in self.text_sections if label == 'a' ]
+        }
+
+    def ann_dict(self):
+        return {
+            'annotations': [ a.to_dict() for a in self.annotations ]
+        }
+
+    def text_json(self):
+        return pretty_dumps(self.text_dict())
+
+    def ann_json(self):
+        return pretty_dumps(self.ann_dict())
+
+    def to_json(self):
+        d = self.text_dict()
+        d.update(self.ann_dict())
+        return pretty_dumps(d)
 
 class LookaheadIterator(collections.Iterator):
     """Lookahead iterator from http://stackoverflow.com/a/1518097."""
@@ -119,34 +206,34 @@ class LookaheadIterator(collections.Iterator):
         return self.lookahead is not None
     
 def is_text_line(line):
-    return TEXT_RE.match(line.rstrip('\n'))
+    return TEXT_RE.match(line.rstrip('\n\r'))
 
 def is_span_line(line):
-    return SPAN_RE.match(line.rstrip('\n'))
+    return SPAN_RE.match(line.rstrip('\n\r'))
 
 def is_rel_line(line):
-    return REL_RE.match(line.rstrip('\n'))
+    return REL_RE.match(line.rstrip('\n\r'))
 
 def read_pubtator_document(fl):
     """Read from LookaheadIterator, return PubTatorDocument."""
     assert isinstance(fl, LookaheadIterator)
     document_id = None
-    document_text = []
+    text_sections = []
     while not fl.lookahead.strip():
         next(fl)    # skip initial empty lines
     for line in fl:
-        m = TEXT_RE.match(line.rstrip('\n'))
+        m = TEXT_RE.match(line.rstrip('\n\r'))
         if not m:
             raise ParseError('%d: %s' % (fl.index, line))
         docid, type_, text = m.groups()
         if document_id is not None and docid != document_id:
             raise ParseError('%d: %s' % (fl.index, line))
         document_id = docid
-        document_text.append(text)
+        if text.strip():
+            text_sections.append((type_, text))
         if not is_text_line(fl.lookahead):
             break
 
-    document_text = '\n'.join(document_text)
     annotations = []
     for line in fl:
         if not line.strip():
@@ -158,7 +245,7 @@ def read_pubtator_document(fl):
         else:
             raise ParseError('%d: %s' % (fl.index, line))
 
-    return PubTatorDocument(document_id, document_text, annotations)
+    return PubTatorDocument(document_id, text_sections, annotations)
 
 def read_pubtator(fl):
     """Read PubTator format from file-like object, yield PubTatorDocuments."""
@@ -167,23 +254,22 @@ def read_pubtator(fl):
     while lines:
         yield read_pubtator_document(lines)
 
-def write_standoff(document, options=None):
+def output_filename(document, suffix, options):
     try:
         outdir = options.output if options.output is not None else ''
     except:
         outdir = ''
-    try:
-        encoding = options.encoding
-    except:
-        encoding = DEFAULT_ENCODING
-    textout = path.join(outdir, document.id + '.txt')
-    annout = path.join(outdir, document.id + '.ann')
-    with codecs.open(textout, 'wt', encoding=encoding) as txt:
+    return path.join(outdir, document.id + suffix)
+
+def write_standoff(document, options=None):
+    textout = output_filename(document, '.txt', options)
+    annout = output_filename(document, '.ann', options)
+    with codecs.open(textout, 'wt', encoding=encoding(options)) as txt:
         txt.write(document.text)
         if not document.text.endswith('\n'):
             txt.write('\n')
     ann_by_id = {}
-    with codecs.open(annout, 'wt', encoding=encoding) as ann:
+    with codecs.open(annout, 'wt', encoding=encoding(options)) as ann:
         for pa_ann in document.annotations:
             if not isinstance(pa_ann, SpanAnnotation):
                 warn('not converting %s' % type(pa_ann).__name__)    # TODO
@@ -191,20 +277,28 @@ def write_standoff(document, options=None):
             for so_ann in pa_ann.to_ann_lines(ann_by_id):
                 print >> ann, so_ann
 
-def pubtator_to_standoff(fn, options=None):
-    try:
-        encoding = options.encoding
-    except:
-        encoding = DEFAULT_ENCODING
-    with codecs.open(fn, 'rU', encoding=encoding) as fl:
+def write_json(document, options=None):
+    outfn = output_filename(document, '.json', options)
+    with codecs.open(outfn, 'wt', encoding=encoding(options)) as out:
+        out.write(document.to_json())
+
+def convert(fn, writer, options=None):
+    with codecs.open(fn, 'rU', encoding=encoding(options)) as fl:
         for document in read_pubtator(fl):
-            write_standoff(document, options)
-        
+            writer(document, options)
+
 def main(argv):
     args = argparser().parse_args(argv[1:])
 
+    if args.format == 'standoff':
+        writer = write_standoff
+    elif args.format == 'json':
+        writer = write_json
+    else:
+        raise ValueError('unknown format %s' % args.format)
+
     for fn in args.files:
-        pubtator_to_standoff(fn, args)
+        convert(fn, writer, args)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
