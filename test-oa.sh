d093800d7abc60340ee54aee223a7742d14456e6
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2016-06-11 22:18:08 +0100

    Update tests

--- test-oa.sh
+++ test-oa.sh
@@ -5,13 +5,13 @@ set -u
 
 # INPUT="data/BioCreative-V-CDR/CDR_TrainingSet.PubTator"
 INPUT="data/samples/bioconcepts2pubtator_offsets.sample"
-OUTDIR="test-output"
+OUTDIR="test-oa-output"
 
 echo "Clearing $OUTDIR" >&2
 rm -rf "$OUTDIR"
 mkdir "$OUTDIR"
 
 echo "Converting $INPUT, output in $OUTDIR" >&2
-python convertpubtator.py -f standoff -o "$OUTDIR" "$INPUT"
+python convertpubtator.py -f oa-jsonld -o "$OUTDIR" "$INPUT"
 
 echo "Done." >&2
