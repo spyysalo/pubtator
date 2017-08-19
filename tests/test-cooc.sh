2f71fb5cdb37baffa6e0fdb0ed9a24dbfd476639
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2017-08-19 23:06:16 +0100

    Add --recurse argument

--- tests/test-cooc.sh
+++ tests/test-cooc.sh
@@ -7,4 +7,4 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DATADIR="$SCRIPTDIR/../data/test-wa-output"
 
 "$SCRIPTDIR/test-wa.sh"
-python "$SCRIPTDIR/../tools/addcoocrelations.py" "$DATADIR"/*/*.jsonld
+python "$SCRIPTDIR/../tools/addcoocrelations.py" -r "$DATADIR"
